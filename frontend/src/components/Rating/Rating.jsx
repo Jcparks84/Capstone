8137af866e7b3dcf6791c7baa72cb276171a94c2
John Parks <jcparks1984@gmail.com>
2022-04-15 13:34:10 -0400

    Fixed Some Issues With Rating Component, Still Troubleshooting If Else Statement

--- frontend/src/components/Rating/Rating.jsx
+++ frontend/src/components/Rating/Rating.jsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 import "./Rating.css";
 
 function StarIcon(props) {
-    const {fill = 'none'} = props;
+    const {fill = {fill}} = props;
     return(
-    <svg class="w-6 h-6" fill="yellow" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
+    <svg class="w-6 h-6" fill= 'none' stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
     );
 }
 
@@ -17,19 +17,22 @@ function RatingIcon(props){
         onMouseEnter,
         onMouseLeave,
         onSaveRating,
-    } =(props)
+    } =(props);
     const fill = React.memo(() => {
         if(hoverRating >= index){
             return 'yellow';
-        } else if (!hoverRating && rating >= index)
+        } else if (!hoverRating && rating >= index){
+            return 'yellow';
+        }
         return 'none';
     },[rating, hoverRating, index]);
+    // console.log(index)
     return(
-        <div 
+        <div className="cursor-pointer"
         onMouseEnter={()=> onMouseEnter(index)} 
-        onMouseLeave={()=> onMouseLeave(index)} 
+        onMouseLeave={()=> onMouseLeave()} 
         onClick={()=> onSaveRating(index)}>
-            <StarIcon fill = {fill}/>
+        <StarIcon fill = {fill}/>
         </div>
     )
 }
@@ -45,16 +48,20 @@ const onMouseEnter = (index) => {
     setHoverRating(index);
 };
 const onMouseLeave = () => {
-    setHoverRating(0)
+    setHoverRating(0);
 };
 const onSaveRating = (index) =>{
-    setRating(index)
-}
+    setRating(index);
+
+    console.log(index)
+};
+
+// console.log(hoverRating)
+
 
     return (
-        <>
-        <p>I am displaying from the Rating component</p>
-            <div className="box">
+        
+            <div className="box flex">
             {[1,2,3,4,5,].map((index)=> {
                
                return(
@@ -68,9 +75,9 @@ const onSaveRating = (index) =>{
             })}
             
         </div>
-        </>
+        
     
     )
 }
 
-export default Rating;    
+export default Rating;
