a159b5b5804449cedac8bb9d0cc72cbe5ab32e80
John Parks <jcparks1984@gmail.com>
2022-07-23 14:02:03 -0400

    Fixed Map Loading Error

---
+++ .history/frontend/src/components/Map/Map_20220722104900.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from "@react-google-maps/api";
+import { formRelative } from "date-fns";
+
+// import usePlacesAutocomplete, {
+//     getGeocode,
+//     getLatLng,
+// } from "use-places-autocomplete"
+// import {
+//     Combobox,
+//     ComboboxInput,
+//     ComboboxPopover,
+//     ComboboxList,
+//     ComboboxOption,
+// } from "@reach/combobox/styles.css"
+
+import mapStyles from "./mapStyles";
+import "./Map.css"
+
+
+const libraies = ['places']
+const mapContainerStyle = {
+    width: '100vh',
+    height: "100vh",
+};
+const center = {
+    lat: 44.7630567,
+    lng: -85.6206317,
+};
+const options = {
+    style: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+    {process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE}
+}
+
+export default function Map() {
+    const{ isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: REACT_APP_GOOGLE_API_KEY,
+        libraies,
+    });
+
+    const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  })
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+
+    return <div>
+        <h3>Breweries <span role='img' aria-label="brewery">🍺</span></h3>
+        <GoogleMap mapContainerStyle = {mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onLoad={onMapLoad}
+        ></GoogleMap>
+    </div>;
+}
